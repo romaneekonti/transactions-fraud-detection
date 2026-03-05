f841a83db4d6b3a5f50953cb5b5c18cbd6de7f0d
romaneekonti <blanarucristian224@gmail.com>
2026-03-06 00:57:51 +0200

    feat: add colors

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@
 
 using namespace std;
 
-string askFileName() {
+void show_file_name_title(string title, string file_name) {
+    int row, col;
+    getmaxyx(stdscr, row, col);
+
+    int start_col = (col - title.length() - file_name.length()) / 2;
+    mvprintw(0, start_col, title.c_str());
+    
+    attron(COLOR_PAIR(1));
+    mvprintw(0, start_col + title.length(), file_name.c_str());
+    attroff(COLOR_PAIR(1));
+    
+    refresh();
+}
+
+string ask_file_name() {
     vector<string> file_array;
 
     for (const auto& entry : filesystem::directory_iterator(".")) {
@@ -17,15 +31,16 @@ string askFileName() {
     int selected_index = 0;
     string return_value = "";
 
-    initscr();
-    keypad(stdscr, true);
     while (true) {
         clear();
         for (int i = 0; i < file_array.size(); i++) {
-            if (i == selected_index)
-                printw(">>[%s]", file_array[i].c_str());
-            else
+            if (i == selected_index) {
+                attron(COLOR_PAIR(1));
                 printw("[%s]", file_array[i].c_str());
+                attroff(COLOR_PAIR(1));
+            } else {
+                printw("[%s]", file_array[i].c_str());
+            }
             printw("\n");
         }
         refresh();
@@ -42,14 +57,24 @@ string askFileName() {
         }
 
     }
-    endwin();
 
     return return_value;
 }
 
 int main() {
-    
-    printf("%s", askFileName().c_str());
+    initscr();
+    keypad(stdscr, true);
 
+    start_color();
+    use_default_colors();
+    init_pair(1, COLOR_GREEN, -1); //green text, default bg
+
+    string file = ask_file_name();
+    clear();
+    show_file_name_title("Selected file: ", file);
+
+    getch();
+
+    endwin();
     return 0;    
 }
